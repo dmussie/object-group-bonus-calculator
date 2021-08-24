9d463765e314334d0e991d3568e7596a0d105475
Danny Mussie <dannymussie7@gmail.com>
2021-08-24 16:26:06 -0500

    Got up to totalBonus step of bonus calculator

--- client.js
+++ client.js
@@ -40,4 +40,62 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+function employeeBonuses (array) {
+  for (item of array) {
+    console.log(item);
+  }
+}
+employeeBonuses(employees);
+// console.table( employees );
+
+function addBonusPercentage (array) {
+  for (let index = 0; index < array.length; index++) {
+    if (employees[index].reviewRating <= 2) {
+      employees[index].bonusPercentage = 0;
+      //console.log(employees[index]);
+    } else if (employees[index].reviewRating === 3) {
+      employees[index].bonusPercentage = 4;
+      //console.log(employees[index]);
+    } else if (employees[index].reviewRating === 4) {
+      employees[index].bonusPercentage = 6;
+      //console.log(employees[index]);
+    } else if (employees[index].reviewRating === 5) {
+      employees[index].bonusPercentage = 10;
+      // console.log(employees[index]);
+    } 
+  }
+  for (let index = 0; index < array.length; index++) {
+    if (employees[index].employeeNumber.length === 4) {
+      employees[index].bonusPercentage += 5; 
+      console.log(employees[index]);
+    }
+  }
+  for (let index = 0; index < array.length; index++) {
+    if (parseInt(employees[index].annualSalary) > 65000) {
+      employees[index].bonusPercentage -= 1;
+      console.log(employees[index]);
+    }
+  }
+  for (let index = 0; index < array.length; index++) {
+    if (employees[index].bonusPercentage < 0) {
+      employees[index].bonusPercentage = 0;
+      // console.log(employees[index]);
+    } else if (employees[index].bonusPercentage > 13) {
+      employees[index].bonusPercentage = 13;
+    }
+  }
+}
+
+function totalBonus (array) {
+  for (let index = 0; index < array.length; index++) {
+    totalBonusCalc = parseInt(employees[index].annualSalary) *;
+  } 
+  return totalBonusCalc;
+}
+
+console.log(totalBonus(employees));
+console.table(employees);
+
+
+addBonusPercentage(employees);
+console.table( employees );
